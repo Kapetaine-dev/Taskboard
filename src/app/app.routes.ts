58e36e8af6d6545c0fbeae5186255d7b164ac25d
Paul <beja.paulm@gmail.com>
2025-11-28 10:51:47 +0100

    premier exo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,       // route d’accueil
+  },
+  {
+    path: 'about',
+    component: About,      // page "À propos"
+  },
+  {
+    path: '**',
+    redirectTo: '',                 
+  },
+];
