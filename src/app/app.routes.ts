78684b60c8f554a6976e49eb28e867f8901ad184
Paul <beja.paulm@gmail.com>
2025-12-18 14:18:32 +0100

    premier test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { About } from './about/about';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: About },
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then(m => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about').then(m => m.About),
+  },
   {
     path: 'tasks',
     loadChildren: () =>
-      import('./features/tasks/tasks-page/route').then((m) => m.TASK_ROUTES),
+      import('./features/tasks/tasks-page/route').then(m => m.TASK_ROUTES),
   },
+  { path: '**', redirectTo: '' },
 ];
